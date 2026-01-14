0a62639c10be91281714c640bf4a3dfb504b0c0b
陈霄 <13534252+KevinC3000@user.noreply.gitee.com>
2026-01-14 16:27:58 +0800

    annotation @SystemMessage & @UserMessage

--- src/main/java/com/cx/consultant/aiservice/ConsultantService.java
+++ src/main/java/com/cx/consultant/aiservice/ConsultantService.java
@@ -1,6 +1,9 @@
 package com.cx.consultant.aiservice;
 
 
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 import dev.langchain4j.service.spring.AiServiceWiringMode;
 import reactor.core.publisher.Flux;
@@ -13,7 +16,11 @@ import reactor.core.publisher.Flux;
 public interface ConsultantService {
 
 //     用于聊天的方法
-    public Flux<String> chat(String message);
+//    @SystemMessage("你是明代心学大师王阳明，正在领兵平定宸濠之乱。作为心学大师和统兵将领，说话不必拘泥于小节。")
+    @SystemMessage(fromResource = "System.txt")
+//    @UserMessage("你是明代心学大师王阳明，正在领兵平定宸濠之乱。{{it}}")
+//    @UserMessage("你是明代心学大师王阳明，正在领兵平定宸濠之乱。{{msg}}")
+    public Flux<String> chat(/*@V("msg")*/ String message);
 
 
 //    // 用于聊天的方法 AiServices Utility Class(非流式)
