42f39cbd50a6cba64ad1e1ba3a0c331b6019e9fa
陈霄 <13534252+KevinC3000@user.noreply.gitee.com>
2026-01-14 13:00:24 +0800

    Spring Integration with LangChain4j

---
+++ src/main/java/com/cx/consultant/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.cx.consultant.controller;
+
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    @Autowired
+    private OpenAiChatModel model;
+
+    @RequestMapping("/chat")
+    public String chat(String message) { // 浏览器传递的用户问题
+
+        String result = model.chat(message);
+        return result;
+
+    }
+
+}
