792b341ef61e974cff0334bbab7932a9b9880893
陈霄 <13534252+KevinC3000@user.noreply.gitee.com>
2026-01-20 23:16:04 +0800

    RAG Knowledge Base: ApachePdfBoxDocumentParser (Core API)

--- src/main/java/com/cx/consultant/config/CommonConfig.java
+++ src/main/java/com/cx/consultant/config/CommonConfig.java
@@ -2,6 +2,9 @@ package com.cx.consultant.config;
 
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.ClassPathDocumentLoader;
+//import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
+import dev.langchain4j.data.document.parser.TextDocumentParser;
+import dev.langchain4j.data.document.parser.apache.pdfbox.ApachePdfBoxDocumentParser;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
@@ -17,6 +20,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
@@ -71,7 +75,30 @@ public class CommonConfig {
     @Bean
     public EmbeddingStore myEmbeddingStore(){// embeddingStore的对象，这个对象的名字不能和依赖自动注入的重复，所以这里要用myEmbeddingStore
 //        1.加载文档进内存
-        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+//        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content");
+//        List<Document> documents = ClassPathDocumentLoader.loadDocuments("content", new ApachePdfBoxDocumentParser());
+
+//        1.1尝试按文件类型显示分流
+        List<Document> documents = new ArrayList<>();
+
+        TextDocumentParser textDocumentParser = new TextDocumentParser();
+
+        //        pdf
+        documents.addAll(
+                ClassPathDocumentLoader.loadDocuments(
+                        "content/pdfs",
+                        new ApachePdfBoxDocumentParser()
+                )
+        );
+
+        //        txt/md
+        documents.addAll(
+                ClassPathDocumentLoader.loadDocuments(
+                        "content/texts",
+                        new TextDocumentParser()
+                )
+        );
+
 //        2.构建向量数据库操作对象
         InMemoryEmbeddingStore store = new InMemoryEmbeddingStore();
 //        3.构建一个EmbeddingStoreIngestor对象
@@ -88,7 +115,8 @@ public class CommonConfig {
     public ContentRetriever contentRetriever(@Qualifier("myEmbeddingStore") EmbeddingStore store){
         return EmbeddingStoreContentRetriever.builder()
                 .embeddingStore(store)
-                .minScore(0.5)
+//                .minScore(0.5)
+                .minScore(0.3)
                 .maxResults(3)
                 .build();
     }
